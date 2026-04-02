e6d6615ed439b281bb284a4f390ffc07c8b57f27
Jorge Luís <jorge_lcampos@hotmail.com>
2026-04-02 09:33:41 -0300

    comitando o desenvolvimento até a aula 24

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -6,10 +9,21 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2='String'>
-        Olá Mundo Novo !!!
-      </Heading>
-      <p>Aqui algum texto !!!</p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <Heading>FORMULÁRIO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>RODAPÉ</Heading>
+      </Container>
     </>
   );
 }
